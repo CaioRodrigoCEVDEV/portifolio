c3e6c0f0020101fed09696c9f622735e956b11c7
Caio Rodrigo <contato@caiorodrigocev.com.br>
2025-09-20 23:31:42 -0300

    Implementado APInode

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require("path");
+const app = express()
+const port = 3003
+
+// raiz do projeto (um nível acima de /src)
+const ROOT = path.resolve(__dirname, "..");
+
+
+// estáticos
+app.use(express.static(path.join(ROOT, "public")));
+app.use("/public", express.static(path.join(ROOT, "public")));
+
+// rotas
+app.get(["/", "/index"], (req, res) => {
+  res.sendFile(path.join(ROOT, "public/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Rodando na porta: ${port}`)
+})
